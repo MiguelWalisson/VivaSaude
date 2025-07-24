b9b8ec3542d8be7a763e35dd0ddf7bb5bfd51fa0
Miguel Walisson <miguelwalisson098@gmail.com>
2025-07-24 17:51:23 -0300

    main
    base inicial do projeto

--- assents/js/cadastroPaciente.js
+++ assents/js/cadastroPaciente.js
@@ -1,29 +1,69 @@
 'use strict';
 
+const limparFormulario = () => {
+    document.getElementById('endereco').value = '';
+
+}
+
+const preencherEndereco = (endereco) =>{
+    document.getElementById('enderco').value = endereco.logradouro.endereco.bairro.endereco.localidade.endereco.uf;
+
+};
+
+const buscarCep = async()=>{
+    try{
+        const cep = document.getElementById('CEP').value;
+        const url = `https://viacep.com.br/ws/${cep}/json/`
+        const regexcep = /^[0-9]{8}$/;
+
+        if(!regexcep.test(cep)){
+            document.getElementById('CEP').value = 'Formato invalido!'
+            console.error("CEP não encontrado!")
+            limparFormulario();
+            return;
+        }
+        const dados = await fetch(url);
+        const endereco = await dados.json();
+        
+        if(endereco.hasOwnProperty('erro')){
+            document.getElementById('CEP').value = 'CEP não encontrado!'
+            console.error("CEP não encontrado!")
+            limparFormulario();
+            return;
+        }
+        preencherEndereco(endereco);
+        console.log(endereco);
+    }catch(error){
+        console.error("Erro ao buscar cep:",error);
+        limparFormulario();
+    }
+};
+document.getElementById('CEP')
+.addEventListener('focusout',buscarCep);
 
 const CadastroPaciente =async(event) => {
     event.preventDefault();
     try{
 
     
-    const nome = document.getElementById('Nome Completo').value = '';
-    const CPF = document.getElementById('CPF').value = '';
-    const dataNascimento = document.getElementById('Data de Nascimento').value = '';
-    const genero = document.getElementById('Gênero').value = '';
-    const telefone = document.getElementById('Telefone').value = '';
-    const CEP = document.getElementById('CEP').value = '';
-    const endereco = document.getElementById('endereco').value = '';
-    const email = document.getElementById('email').value = '';
-    const senha = document.getElementById('Senha').value = '';
-    const senhaConfirm = document.getElementById('Confirmar Senha').value = '';
+    const nome = document.getElementById('Nome Completo').value;
+    const cpf = document.getElementById('CPF').value = '';
+    const dataNascimento = document.getElementById('Data de Nascimento').value;
+    const genero = document.getElementById('Gênero').value;
+    const telefone = document.getElementById('Telefone').value;
+    const cep = document.getElementById('CEP').value;
+    const endereco = document.getElementById('endereco').value;
+    const email = document.getElementById('email').value;
+    const senha = document.getElementById('Senha').value;
+    const senhaConfirm = document.getElementById('Confirmar Senha').value;
     
     const DadosPaciente = {
         nome: nome,
-        CPF: CPF,
+        cpf: cpf,
         dataNascimento: dataNascimento,
         genero: genero,
         telefone: telefone,
-        CEP: CEP,
+        cep: cep,
         endereco: endereco,
         email: email,
         senha: senha,
@@ -37,13 +77,16 @@ console.log('Enviando dados para API:', DadosPaciente);
         },
         body: JSON.stringify(DadosPaciente)
     })
+     if (responsePaciente.ok){
+             CadastroPaciente = await apiresposta.json();
+            alert("Funcionario Cadastrado!");
+            document.getElementById('formcadastroFuncionario').reset();
+        } else{
+            const erroTexto = await apiresposta.text();
+            alert("Erro ao cadastrar funcionário :" + erroTexto);
+        }
 }catch (error) {
         console.error('Erro error');
 }
 };
-CadastrarPaciente.addEventListener('click', (event) => {
-    event.preventDefault();
-    CadastrarPaciente();
-    alert('Cadastro realizado com sucesso!');
-});
 
