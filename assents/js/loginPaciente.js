7bdc4a4dea4a90fcfb2045e865a7bb787767d10d
Miguel Walisson <miguelwalisson098@gmail.com>
2025-08-09 15:37:04 -0300

    loginMedico

--- assents/js/loginPaciente.js
+++ assents/js/loginPaciente.js
@@ -1,6 +1,9 @@
 'use strict'
 
 const url = 'http://localhost:8080/auth/login';
+const urlParams = new URLSearchParams(window.location.search);
+const tipo = urlParams.get('tipo');
+
 async function loginPaciente(event){
     event.preventDefault();
     const email = document.getElementById('email').value;
@@ -10,7 +13,7 @@ async function loginPaciente(event){
         const response = await fetch(url, {
             method: 'POST',
             headers: {"Content-Type": "application/json"},
-            body: JSON.stringify({email, senha})
+            body: JSON.stringify({email, senha, tipo})
         });
         if(response.ok){
             const paciente = await response.json();
